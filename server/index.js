df113cb7aec74d2cffead6c413330f70e6b65920
rithin69 <69008632+rithin69@users.noreply.github.com>
2024-08-22 00:27:07 +0100

    updd

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
-// Modules
 const http = require('http');
 const push = require('./push');
 
 // Create HTTP Server
 http.createServer((request, response) => {
-
   // Enable CORS for cross-origin requests
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -22,56 +20,30 @@ http.createServer((request, response) => {
 
   // Handle subscription requests
   if (method === 'POST' && url.match(/^\/subscribe\/?/)) {
-
-    // Get the POST Body data
     let body = [];
-
-    // Read the body data stream
     request.on('data', chunk => body.push(chunk)).on('end', () => {
-
-      // Parse the subscription body to an object
       let subscription = JSON.parse(body.toString());
-
-      // Log the subscription data
       console.log('New Subscription:', subscription);
-
-      // Store the subscription for push notifications
       push.addSubscription(subscription);
-
-      // Respond with a success message
       response.end('Subscribed');
     });
-
+  }
   // Handle requests to retrieve the VAPID public key
-  } else if (url.match(/^\/key\/?/)) {
-
-    // Fetch the public VAPID key
+  else if (method === 'GET' && url.match(/^\/key\/?/)) {
     const vapidKey = push.getKey();
-
-    // Log the public VAPID key
     console.log('VAPID Public Key:', vapidKey);
-
-    // Respond with the public key
     response.end(vapidKey);
-
+  }
   // Handle requests to send a push notification
-  } else if (method === 'POST' && url.match(/^\/push\/?/)) {
-
-    // Get the POST Body data
+  else if (method === 'POST' && url.match(/^\/push\/?/)) {
     let body = [];
-
-    // Read the body data stream
     request.on('data', chunk => body.push(chunk)).on('end', () => {
-
-      // Send the push notification with the body data
       push.send(body.toString());
-
-      // Respond with a success message
       response.end('Push Sent');
     });
-
+  }
   // Handle unknown requests (404 Not Found)
-  } else {
+  else {
     response.statusCode = 404;
     response.end('Error: Unknown Request');
   }
